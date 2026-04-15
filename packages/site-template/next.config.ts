34a470c546177762be6105b7516bacdba8f4cb80
Het Shah <20669099@latrobe.students.edu.au>
2026-04-15 18:00:29 +1000

    Remove non-existent @exodus/bytes dependency and config

--- packages/site-template/next.config.ts
+++ packages/site-template/next.config.ts
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config) => {
-    config.externals.push({
-      '@exodus/bytes': 'commonjs @exodus/bytes'
-    })
-    return config
-  }
 }
 
 module.exports = nextConfig
