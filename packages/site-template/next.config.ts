c50d1920703d0dd63d38843c96c384f72a14061f
Het Shah <20669099@latrobe.students.edu.au>
2026-04-15 17:53:23 +1000

    Add webpack configuration for ESM module compatibility

--- packages/site-template/next.config.ts
+++ packages/site-template/next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.externals.push({
+      '@exodus/bytes': 'commonjs @exodus/bytes'
+    })
+    return config
+  }
 }
 
 module.exports = nextConfig
